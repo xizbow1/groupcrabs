d850b4a4e91e38fedcec2da5f5edb6ec35ac75ef
rwrftr <roanrafter@gmail.com>
2023-10-05 09:13:29 -0700

    Created moveCapt
    created moveCapt and added calls to it in crabs

--- crabs.m
+++ crabs.m
@@ -4,14 +4,14 @@ function crabs ()
 % hunts for a very clever and powerful crab.
 % Draw the game map and initialize map dimensions.
 
-[mapHeight , mapWidth] = drawMap( "BGImage.png" );
+  [mapHeight , mapWidth] = drawMap( "BGImage.png" );
 
 % Initialize captain location, heading and size
 
-xCapt = 1000;
-yCapt = 1000;
-thetaCapt = -pi/2;
-sizeCapt = 50;
+  xCapt = 1000;
+  yCapt = 1000;
+  thetaCapt = pi/2;
+  sizeCapt = 50;
 
 % Draw the captain and initialize graphics handles
 
@@ -19,7 +19,38 @@ sizeCapt = 50;
 % Put your call to drawCapt() here ..... You must give drawCapt its
 % input and output arguments.
 
-captainGraphics = drawCaptain(xCapt, yCapt, thetaCapt, sizeCapt)
+  captainGraphics = drawCaptain(xCapt, yCapt, thetaCapt, sizeCapt);
+ 
+  %initial command 
+  
+  cmd="null";
+  
+  while(cmd != "Q")
+    
+    % read keyboard
+    cmd = kbhit();
+    
+    if( cmd == "w" || cmd == "a" ||  cmd == "d" )
+     
+     % erase old captain
+     for i = 1:length(captainGraphics)
+        set(captainGraphics(i),'Visible','off');  
+     endfor
+    
+    % move captain 
+    
+    [xCapt,yCapt,thetaCapt] = moveCapt(cmd,xCapt,yCapt,thetaCapt);
+    
+    % draw new captain
+    
+    captainGraphics = drawCaptain(xCapt, yCapt, thetaCapt, sizeCapt);
+    
+    endif
+    
+    
+    
+  endwhile
+  
 
-%*******************************************************
+close all
 endfunction

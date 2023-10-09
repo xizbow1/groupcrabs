de9d1e8d24740dfa81a0b61bd95520149286693c
rwrftr <roanrafter@gmail.com>
2023-10-09 14:56:56 -0700

    update crabs.m
    runs the same, minor formatting changes

--- crabs.m
+++ crabs.m
@@ -1,24 +1,24 @@
 function crabs ()
-  
-% Crabs is a kids computer game where a fisherman, called the captain,
-% hunts for a very clever and powerful crab.
-% Draw the game map and initialize map dimensions.
 
-  [mapHeight , mapWidth] = drawMap( "BGImage.png" );
+ % Crabs is a kids computer game where a fisherman, called the captain,
+ % hunts for a very clever and powerful crab.
+ % Draw the game map and initialize map dimensions.
 
-% Initialize captain location, heading and size
+[mapHeight , mapWidth] = drawMap( "BGImage.png" );
 
-  xCapt = 1000;
-  yCapt = 1000;
-  thetaCapt = pi/2;
-  sizeCapt = 50;
+ % Initialize captain location, heading and size
 
-% Draw the captain and initialize graphics handles
+ xCapt = 1000;
+ yCapt = 1000;
+ thetaCapt = -pi/2;
+ sizeCapt = 50;
+
+ % Draw the captain and initialize graphics handles
 
   captainGraphics = drawCaptain(xCapt, yCapt, thetaCapt, sizeCapt);
  
   %initial command 
-  cmd="null";
+  cmd ="null";
   
   while(cmd != "Q")
     

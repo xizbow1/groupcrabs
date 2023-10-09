2fe1535350e99f84d14a296a3a81b59383c10870
rwrftr <roanrafter@gmail.com>
2023-10-09 13:23:39 -0700

    updated crabs.m to allow the captain to be moved
    replaced some code in "crabs.m" file with code from my own "crabs.m"
    
    note: the captain is now initially drawn upside-down for some reason.

--- crabs.m
+++ crabs.m
@@ -1,44 +1,51 @@
 function crabs ()
 
-% Crabs is a kids computer game where a fisherman, called the captain,
-% hunts for a very clever and powerful crab.
-% Draw the game map and initialize map dimensions.
+ % Crabs is a kids computer game where a fisherman, called the captain,
+ % hunts for a very clever and powerful crab.
+ % Draw the game map and initialize map dimensions.
 
 [mapHeight , mapWidth] = drawMap( "BGImage.png" );
 
-% Initialize captain location, heading and size
+ % Initialize captain location, heading and size
 
-xCapt = 1000;
-yCapt = 1000;
-thetaCapt = -pi/2;
-sizeCapt = 50;
+ xCapt = 1000;
+ yCapt = 1000;
+ thetaCapt = -pi/2;
+ sizeCapt = 50;
 
-% Draw the captain and initialize graphics handles
+ % Draw the captain and initialize graphics handles
 
-%*********************************************************
-% Put your call to drawCapt() here ..... You must give drawCapt its
-% input and output arguments.
-
-captainGraphics = drawCaptain(xCapt, yCapt, thetaCapt, sizeCapt)
-
-cmd="null";
-
-while(cmd != "Q")
-
-  cmd = kbhit()
-
-  if(cmd == "w" || cmd == "a" || cmd == "d")
-    % erase old captain
-    for i=1:length(captainGraphics)
-      set(captainGraphics(i),'Visible','off');
-    endfor
-
-    %move captain
+  captainGraphics = drawCaptain(xCapt, yCapt, thetaCapt, sizeCapt);
+ 
+  %initial command 
+  cmd="null";
   
-    [xCapt, yCapt, thetaCapt] = moveCapt(cmd,xCapt,yCapt,thetaCapt);  
+  while(cmd != "Q")
+    
+    % read keyboard
+    cmd = kbhit();
+    
+    if( cmd == "w" || cmd == "a" ||  cmd == "d" || cmd == "s")
+     
+     % erase old captain
+     for i =1:length(captainGraphics)
+        set(captainGraphics(i),'Visible','off');  
+     endfor
+    
+    % move captain 
+    
+    [xCapt,yCapt,thetaCapt] = moveCapt(cmd,xCapt,yCapt,thetaCapt);
+    
+    % draw new captain
+    
+    captainGraphics = drawCaptain(xCapt, yCapt, thetaCapt, sizeCapt);
+    
+    endif
+    
+    
+    
+  endwhile
+  
 
-  endif
-endwhile
-
-%*******************************************************
+close all
 endfunction

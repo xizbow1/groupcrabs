bb38c3a3320b5265f3e208f1e09044b76ffa02ea
rwrftr <roanrafter@gmail.com>
2023-10-20 13:03:29 -0700

    added drawJelly and getJelly

---
+++ getJelly.m
@@ -0,0 +1,27 @@
+## Copyright (C) 2023 Roan Rafter
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+## -*- texinfo -*-
+## @deftypefn {} {@var{retval} =} getJelly (@var{input1}, @var{input2})
+##
+## @seealso{}
+## @end deftypefn
+
+## Author: Roan Rafter <roan@Kevins-MacBook-Pro-4.local>
+## Created: 2023-10-20
+
+function retval = getJelly (input1, input2)
+
+endfunction

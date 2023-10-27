177689a71755ad777d071c3a427234feffb7c537
rwrftr <roanrafter@gmail.com>
2023-10-27 13:29:01 -0700

    adding net to captain WIP

--- drawCaptain.m
+++ drawCaptain.m
@@ -34,6 +34,11 @@ pt11=capt( : , 11);
 pt12=capt( : , 12);
 pt13=capt( : , 13);
 pt14=capt( : , 14);
+pt15=capt( : , 15);
+pt16=capt( : , 16);
+pt17=capt( : , 17);
+pt18=capt( : , 18);
+
 
 % Draw the captain and set the return vector of graphics handles.
 captainGraphics(1) = drawLine(pt1 , pt2 , "k");
@@ -48,4 +53,8 @@ captainGraphics(9) = drawLine(pt9 , pt10 , "k");
 captainGraphics(10) = drawLine(pt1 , pt11 , "k");
 captainGraphics(11) = drawLine(pt6 , pt12 , "k");
 captainGraphics(12) = drawLine(pt13 , pt14 , "k");
+captainGraphics(13) = drawLine(pt14 , pt15 , "k");
+captainGraphics(14) = drawLine(pt15 , pt16 , "k");
+captainGraphics(15) = drawLine(pt16 , pt17 , "k");
+captainGraphics(16) = drawLine(pt17 , pt18 , "k");
 endfunction

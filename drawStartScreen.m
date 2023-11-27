7ca04f096cebf076b2b243ed55edd6655ce80d75
rwrftr <roanrafter@gmail.com>
2023-11-27 13:30:57 -0800

    updated drawStartScreen and getLevel

--- drawStartScreen.m
+++ drawStartScreen.m
@@ -2,9 +2,14 @@ function level = drawStartScreen (imgName)
   
   drawMap(imgName);
   hold on
+    messageLoc = [450,750];
+    myMessage = ['Enter a Level 1-9 '];
+    myText = text(messageLoc(1), messageLoc(2), myMessage, 'FontSize', 30, 'Color', 'red');
     level = getLevel();
-    levelLoc = [400,800];
-    %text(LevelLoc(1), LevelLoc(2), strcat())
+    delete(myText)
+    myMessage = ['OK, Level = ', ' ', num2str(level)];
+    text(messageLoc(1), messageLoc(2), myMessage, 'FontSize', 30, 'Color', 'red');
     pause(2)
+   
   hold off
 endfunction
--- getLevel.m
+++ getLevel.m
@@ -1,6 +1,9 @@
 function level = getLevel ( )
   while(1)
+  
+      commandwindow();
       cmd = kbhit(1);
+      
       if(cmd == "1" || cmd == "2" || cmd == "3" || cmd == "4" || cmd == "5" || cmd == "6" || cmd == "7")
         level = str2num(cmd);
         break;    
@@ -8,4 +11,5 @@ function level = getLevel ( )
       
       pause(.1)
     endwhile
+    
 endfunction

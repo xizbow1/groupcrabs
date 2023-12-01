1fe8e134a46ad34c76c40616227ee7bb13f7bfbe
bfc3 <143015695+bfc3@users.noreply.github.com>
2023-12-01 13:11:48 -0800

    Update drawEndScreen.m

--- drawEndScreen.m
+++ drawEndScreen.m
@@ -1,33 +1,44 @@
-function retval = drawEndScreen (imgName, crabsCaught, numCrabs)
-  
+function restart = drawEndScreen (imgName,crabsCaught,numCrabs)
+
+%draw the end screen
   drawMap(imgName);
-  
-  hold on
-    myMessage = ['Yay! you caught ', num2str(crabsCaught), '/', num2str(numCrabs), ' crabs!'];
-    textLoc1 = [350,750];
-    textHandle = text(textLoc1(1), textLoc1(2), myMessage, 'FontSize', 30, 'Color', 'red');
-    
-    pause(5)
-    
-    delete(textHandle)
-    textLoc2 = [450, 750];
-    text(textLoc2(1), textLoc2(2), "do you want to play again? y/n", 'FontSize', 40, 'Color', 'red');
-    
-    % Read Keyboard
-    while (1)
-      
-      commandwindow();
-      cmd=kbhit(1);
-      
-      if (cmd == "y" || cmd == "Y")
-        restart = 1;
-        break;
-      elseif (cmd == "n" || cmd == "N")
-        restart == 0;
-        break;
-      endif
-    endwhile
-  hold off
-  
-  
+
+     hold on
+       myMessage=['Good Job! You Caught  ', num2str(crabsCaught), ' out of ', …
+                              num2str(numCrabs),' crabs!' ];
+       textLoc = [350,750];
+       textHandle=text(textLoc(1), textLoc(2), myMessage, 'FontSize', 30, 'Color', 'red');
+
+       pause(5)
+
+       delete(textHandle)
+       textLoc = [450,750];
+       textHandle=text(textLoc(1), textLoc(2), 'Play again? Y/N', 'FontSize', 40, 'Color', 'red');
+
+       %read keyboard
+        while (1)
+
+          %focus on command window
+          commandwindow();
+
+          cmd=kbhit(1);
+
+          if( cmd == 'y' || cmd == 'Y')
+                restart = 1;
+                break;
+          elseif(cmd == 'n' || cmd == 'N')
+                restart = 0;
+                break;
+          endif
+
+          pause(.1)
+
+        endwhile
+
+
+     hold off
+
+
 endfunction
+
+

85f205303c45001d3a7742cae9aa16e0a28e77f7
bfc3 <143015695+bfc3@users.noreply.github.com>
2023-12-01 13:18:02 -0800

    Add calls to endscreen

--- crabs.m
+++ crabs.m
@@ -3,20 +3,20 @@ function crabs ()
  % Crabs is a kids computer game where a fisherman, called the captain,
  % hunts for a very clever and powerful crab.
  % Draw the game map and initialize map dimensions.
-    
- %initial command    
+
+ %initial command
  cmd ="null";
- 
+
   playgame = 1;
   while(playgame && cmd != "Q")
 
    level = drawStartScreen( "crowSprite.png" );
-   
+
    [mapHeight , mapWidth] = drawMap( "BGImage.png" );
 
    numCrabs = level;
    numJellies = level;
-   
+
    % Initialize captain location, heading and size
       xCapt = 1000;
       yCapt = 200;
@@ -72,7 +72,7 @@ function crabs ()
       healthStatus = text(healthLoc(1), healthLoc(2), strcat('Health = ', num2str(healthCapt)), 'FontSize', 12, 'Color', 'red');
 
       catchStatus = text(catchLoc(1), catchLoc(2), strcat('Crabs Caught = ', num2str(catches)), 'FontSize', 12, 'Color', 'red');
-    
+
       % check for jelly collision with capt
       for j = 1:numJellies
         if ( getDist(xJelly(j),yJelly(j),xCapt,yCapt) < 3*sizeCapt )
@@ -86,23 +86,23 @@ function crabs ()
           delete(jellyGraphics(j,i));
         endfor
       endfor
-    
+
       % move jellyfish
       for j = 1:numJellies
         [xJelly(j),yJelly(j),thetaJelly(j)] = moveJelly(level, xJelly(j), yJelly(j),thetaJelly(j), sizeJelly, mapHeight,mapWidth);
-        
+
         % draw jellyfish
         jellyGraphics(:,j) = drawJelly(xJelly(j),yJelly(j),thetaJelly(j),sizeJelly);
       endfor
       if((cmd == "Q"))
         break
       endif
-      
+
       % read keyboard
       commandwindow();
       cmd = kbhit(1);
       if( cmd == "w" || cmd == "a" ||  cmd == "d" || cmd == "s")
-      
+
         % erase old captain
         for i =1:length(captainGraphics)
           delete(captainGraphics(i));
@@ -119,7 +119,7 @@ function crabs ()
       endif
 
       for k=1:numCrabs
-          
+
           if( !isCrabCaught(k) && getDist(xNet,yNet,xCrab(k),yCrab(k)) < 7*sizeCapt )
 
             % erase the old crab as already done in crabs
@@ -129,37 +129,39 @@ function crabs ()
 
             % compute the crab’s angle to the net with getTheta and the components suggested above
               thetaCrab(k) = getTheta(xNet - xCrab(k), yNet - yCrab(k));
-          
+
             % call moveCrab(). To move backwards use cmd =”k”
               [xCrab(k), yCrab(k), thetaCrab(k)] = moveCrab("k", xCrab(k), yCrab(k), thetaCrab(k), mapWidth, mapHeight);
 
             % draw the crab as already done in crabs
               crabGraphics(:,k) = drawCrab(xCrab(k), yCrab(k), thetaCrab(k), sizeCrab);
           endif
-          
+
         endfor
-    
-      for k = 1:numCrabs 
-      
+
+      for k = 1:numCrabs
+
         if(!isCrabCaught(k) && (getDist(xNet,yNet,xCrab(k),yCrab(k)) < 2*sizeCapt ))%crab is caught
           %keep track of how many crabs are caught
             isCrabCaught(k)=1;
             catches = catches +1;
-          
+
           %erase old crab
             for i=1:length(crabGraphics(:,k))
               delete(crabGraphics(i,k));
             endfor
-            
+
         endif
-      
+
       endfor
 
-    
+
       fflush(stdout);
       pause(.01)
 
     endwhile
+
+    playGame = drawEndScreen("endScreen.png",crabsCaught,numCrabs);
 
 
 
